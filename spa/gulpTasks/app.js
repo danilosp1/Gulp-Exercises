59605bbe70b2410d6d5647837c2ffaf5e119ecb6
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2022-01-30 21:20:47 -0300

    Creating tasks (SPA)

---
+++ spa/gulpTasks/app.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+
+function appHtml(cb){
+    return cb()
+}
+
+function appCss(cb){
+    return cb()
+}
+
+function appJs(cb){
+    return cb()
+}
+
+function appImg(cb){
+    return cb()
+}
+
+module.exports = {
+    appHtml,
+    appCss,
+    appJs,
+    appImg
+}
---
+++ spa/gulpTasks/deps.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+
+function depsCss(cb){
+    return cb()
+}
+
+function depsFonts(cb){
+    return cb()
+}
+
+module.exports = {
+    depsCss,
+    depsFonts
+}
